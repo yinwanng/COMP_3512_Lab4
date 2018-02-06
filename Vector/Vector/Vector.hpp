3538933e7ac87ceb2fb057d4eb18a773ced18e86
yinwanng <xxjohnnyng@gmail.com>
2018-02-06 08:34:46 -0800

    Vector stores three doubles called x,y,z.

---
+++ Vector/Vector/Vector.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class Vector
+{
+public:
+private:
+	double x;
+	double y;
+	double z;
+};
---
+++ Vector/Vector/Vectortester.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Lab 4" << endl;
+
+	system("pause");
+	return 0;
+}
